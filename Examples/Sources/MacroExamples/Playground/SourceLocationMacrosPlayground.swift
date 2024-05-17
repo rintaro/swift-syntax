ec5dd954d964d19c84b1c5437c2a7136bba61a2f
Rintaro Ishizaki <rishizaki@apple.com>
2024-05-17 11:04:35 -0700

    [Macros] Fix column calculation in MacroExpansionContext for plugins
    When retriving a location for a node at the first line, column
    calculation was incorrect. 'column' of 'PluginMessage.Syntax' is
    1-based, so we need to '-1' when using it as a offset.

---
+++ Examples/Sources/MacroExamples/Playground/SourceLocationMacrosPlayground.swift
@@ -0,0 +1,8 @@
+import MacroExamplesInterface
+
+func runSourceLoctionMacrosPlayground() {
+  print("FileID: \(#FileID as String)")
+  print("FilePath: \(#FilePath as String)")
+  print("Line: \(#Line as Int)")
+  print("Column: \(#Column as Int)")
+}
